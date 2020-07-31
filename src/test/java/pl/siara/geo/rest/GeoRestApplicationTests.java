7c0955ae147a77c6d95d80ab25dc7eae7caf329c
lsiara <lsiara@zdalnyserwis.local>
2020-07-31 21:13:17 +0200

    Initial commit

---
+++ src/test/java/pl/siara/geo/rest/GeoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.siara.geo.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
